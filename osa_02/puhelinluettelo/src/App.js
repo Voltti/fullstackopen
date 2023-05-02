76a54539ec5b1d5cae404085b567e8fe22248ef8
Risto Mattila <risto.mattila@brightstraining.com>
2023-05-02 12:20:47 +0300

    Tehtävä 2.10

--- osa_02/puhelinluettelo/src/App.js
+++ osa_02/puhelinluettelo/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import PersonsList from './components/PersonsList'
+import Filter from './components/Filter'
+import AddNew from './components/addNew'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -30,21 +32,10 @@ const addName = (event) => {
     <div>
       <h2>Phonebook</h2>
       
-      <div>filter shown with: <input value={searchTerm} onChange={handleSearchChange} /></div>
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
 
-      <h2>add a new</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button onClick={addName} type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+      <AddNew addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
+
       <PersonsList persons={persons} search={searchTerm} />
     </div>
   )
--- osa_02/puhelinluettelo/src/components/PersonsList.js
+++ osa_02/puhelinluettelo/src/components/PersonsList.js
@@ -1,10 +1,12 @@
 
 const PersonsList = ({persons, search}) => {
-    const filteredPersons = persons.filter(p => p.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    const filteredPersons = persons.filter(p => p.name.toLocaleLowerCase().includes(search.toLocaleLowerCase() ))
     return (
+    <><h2>Numbers</h2>
     <ul>
         {filteredPersons.map( p => <Person key={p.id} name={p.name} number={p.number} />)}
     </ul>
+    </>
     )
 }
 const Person = ({name, number}) => {
