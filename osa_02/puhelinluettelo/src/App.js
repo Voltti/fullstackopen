8d224718daff50c50a8d896507b803228afc418c
Risto Mattila <risto.mattila@brightstraining.com>
2023-05-02 16:28:51 +0300

    Tehtävä 2.11

--- osa_02/puhelinluettelo/src/App.js
+++ osa_02/puhelinluettelo/src/App.js
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonsList from './components/PersonsList'
 import Filter from './components/Filter'
 import AddNew from './components/addNew'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: '040-1231244' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect( () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then( response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
