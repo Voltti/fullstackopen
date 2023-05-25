eaa05a0118c76f7f66ae54f1bc029220225c49ef
Petri Mattila <petri.r.mattila@helsinki.fi>
2023-05-25 14:03:10 +0300

    Tehtävä 3.2

--- osa_03/puhelinluettelo_backend/app.js
+++ osa_03/puhelinluettelo_backend/app.js
@@ -11,6 +11,11 @@ app.get('/api/persons', (req, res) => {
   res.json(numerot);
 });
 
+app.get('/api/info', (req, res) => {
+  res.send(`Phone book has info for ${numerot.length} people. <br />
+    ${Date()}`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
