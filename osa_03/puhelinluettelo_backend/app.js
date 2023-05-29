ea41c88e4e9a547057acb7bc3dcbcdaadbf290c5
Petri Mattila <petri.r.mattila@helsinki.fi>
2023-05-29 16:02:20 +0300

    3.10 korjaus

--- osa_03/puhelinluettelo_backend/app.js
+++ osa_03/puhelinluettelo_backend/app.js
@@ -22,6 +22,10 @@ const generateID = () => {
   return Math.round(Math.random() * 1000000);
 };
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
 app.get('/api/persons', (req, res) => {
   res.json(numerot);
 });
